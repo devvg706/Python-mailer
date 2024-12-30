f0700191315d18fcc26bac6de619a80901a65889
vijay ghindodey <vijayghindodey2@gmail.com>
2024-12-30 22:29:38 +0530

    second commit

--- script.py
+++ script.py
@@ -8,18 +8,14 @@ def send_email(workflow_name, repo_name, workflow_run_id):
     sender_password = os.environ.get('SENDER_PASSWORD')
     receiver_email = os.environ.get('RECEIVER_EMAIL')
 
-
     subject = f"Workflow {workflow_name} for {repo_name} has failed"
-    body = f"Hii, Workflow {workflow_name} for {repo_name} has failed. You can check the logs for more
-    details. \n\n Workflow Run ID: {workflow_run_id}"
+    body = f"Hi, Workflow {workflow_name} for {repo_name} has failed. You can check the logs for more details.\n\nWorkflow Run ID: {workflow_run_id}"
 
     msg = MIMEMultipart()
     msg['From'] = sender_email
     msg['To'] = receiver_email
     msg['Subject'] = subject
     msg.attach(MIMEText(body, 'plain'))
-
-
 
     try:
         server = smtplib.SMTP('smtp.gmail.com', 587)
@@ -30,7 +26,7 @@ def send_email(workflow_name, repo_name, workflow_run_id):
         print('Mail sent successfully')
     except Exception as e:
         print(f'Error: {e}')
+    finally:
         server.quit()
 
-
-        send_email(os.getenv('WORKFLOW_NAME'), os.getenv('REPO_NAME'), os.getenv('WORKFLOW_RUN_ID'))
+send_email(os.getenv('WORKFLOW_NAME'), os.getenv('REPO_NAME'), os.getenv('WORKFLOW_RUN_ID'))
