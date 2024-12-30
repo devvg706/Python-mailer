d8f5860150f1527799e9c0fbc9d1390ce717603f
vijay ghindodey <vijayghindodey2@gmail.com>
2024-12-30 22:43:38 +0530

    third commit

--- script.py
+++ script.py
@@ -4,25 +4,25 @@ from email.mime.multipart import MIMEMultipart
 import os
 
 def send_email(workflow_name, repo_name, workflow_run_id):
-    sender_email = os.environ.get('SENDER_EMAIL')
-    sender_password = os.environ.get('SENDER_PASSWORD')
-    receiver_email = os.environ.get('RECEIVER_EMAIL')
+    senders_email = os.environ.get('senders_EMAIL')
+    senders_password = os.environ.get('senders_PASSWORD')
+    receivers_email = os.environ.get('RECEIVER_EMAIL')
 
     subject = f"Workflow {workflow_name} for {repo_name} has failed"
     body = f"Hi, Workflow {workflow_name} for {repo_name} has failed. You can check the logs for more details.\n\nWorkflow Run ID: {workflow_run_id}"
 
     msg = MIMEMultipart()
-    msg['From'] = sender_email
-    msg['To'] = receiver_email
+    msg['From'] = senders_email
+    msg['To'] = receivers_email
     msg['Subject'] = subject
     msg.attach(MIMEText(body, 'plain'))
 
     try:
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
-        server.login(sender_email, sender_password)
+        server.login(senders_email, senders_password)
         text = msg.as_string()
-        server.sendmail(sender_email, receiver_email, text)
+        server.sendmail(senders_email,receivers_email, text)
         print('Mail sent successfully')
     except Exception as e:
         print(f'Error: {e}')
