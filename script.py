a3e3f8091c32aef3447dc1ff0fa73c420af38908
vijay ghindodey <vijayghindodey2@gmail.com>
2024-12-30 22:09:58 +0530

    firstcommit

---
+++ script.py
@@ -0,0 +1,36 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import os
+
+def send_email(workflow_name, repo_name, workflow_run_id):
+    sender_email = os.environ.get('SENDER_EMAIL')
+    sender_password = os.environ.get('SENDER_PASSWORD')
+    receiver_email = os.environ.get('RECEIVER_EMAIL')
+
+
+    subject = f"Workflow {workflow_name} for {repo_name} has failed"
+    body = f"Hii, Workflow {workflow_name} for {repo_name} has failed. You can check the logs for more
+    details. \n\n Workflow Run ID: {workflow_run_id}"
+
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(sender_email, sender_password)
+        text = msg.as_string()
+        server.sendmail(sender_email, receiver_email, text)
+        print('Mail sent successfully')
+    except Exception as e:
+        print(f'Error: {e}')
+        server.quit()
+
+
+        send_email(os.getenv('WORKFLOW_NAME'), os.getenv('REPO_NAME'), os.getenv('WORKFLOW_RUN_ID'))
