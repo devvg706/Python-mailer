350154cc1c74be9891b561d68a57851582a86a0f
vijay ghindodey <vijayghindodey2@gmail.com>
2024-12-30 22:50:26 +0530

    third commit

--- script.py
+++ script.py
@@ -4,16 +4,19 @@ from email.mime.multipart import MIMEMultipart
 import os
 
 def send_email(workflow_name, repo_name, workflow_run_id):
-    senders_email = os.environ.get('senders_EMAIL')
-    senders_password = os.environ.get('senders_PASSWORD')
-    receivers_email = os.environ.get('RECEIVER_EMAIL')
+    senders_email = os.environ.get('SENDERS_EMAIL')
+    senders_password = os.environ.get('SENDERS_PASSWORD')
+    recievers_email = os.environ.get('RECIEVERS_EMAIL')
+
+    if not senders_email or not senders_password or not recievers_email:
+        raise ValueError("One or more environment variables are missing")
 
     subject = f"Workflow {workflow_name} for {repo_name} has failed"
     body = f"Hi, Workflow {workflow_name} for {repo_name} has failed. You can check the logs for more details.\n\nWorkflow Run ID: {workflow_run_id}"
 
     msg = MIMEMultipart()
     msg['From'] = senders_email
-    msg['To'] = receivers_email
+    msg['To'] =recievers_email
     msg['Subject'] = subject
     msg.attach(MIMEText(body, 'plain'))
 
@@ -22,11 +25,11 @@ def send_email(workflow_name, repo_name, workflow_run_id):
         server.starttls()
         server.login(senders_email, senders_password)
         text = msg.as_string()
-        server.sendmail(senders_email,receivers_email, text)
+        server.sendmail(senders_email,recievers_email, text)
         print('Mail sent successfully')
     except Exception as e:
         print(f'Error: {e}')
     finally:
         server.quit()
 
-send_email(os.getenv('WORKFLOW_NAME'), os.getenv('REPO_NAME'), os.getenv('WORKFLOW_RUN_ID'))
+send_email(os.getenv('workflow_name'), os.getenv('repo_name'), os.getenv('workflow_run_id'))
